e1efdb22e163deaae21cf20da827c47a52d57012
PaaavelZ <paaavel.2024@gmail.com>
2022-03-10 11:34:28 +0300

    Add some init SQL code

---
+++ Code/SQL/chinese_cafeteria.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE chinese_stolovaya;
+USE chinese_stolovaya;
+
+CREATE TABLE products(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20);
+	consistuent TEXT,
+	callories INT,
+	price INT
+);
+
+CREATE TABLE orders(
+	id SERIAL PRIMARY KEY,
+	status_id BIGINT,
+	FOREIGN KEY(status_id) REFERENCES status(id),
+	total TEXT
+);
+
+CREATE TABLE status(
+	id SERIAL PRIMARY KEY,
+	condition TEXT
+);
