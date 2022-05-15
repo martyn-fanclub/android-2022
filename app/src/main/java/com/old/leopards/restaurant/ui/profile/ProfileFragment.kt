a8d748da63d59a4f6f1194c34d2d5476fc58cf11
mrboxmen1 mrboxmen1 <Artyom>
2022-05-15 14:12:15 +0300

    Fix bugs

--- app/src/main/java/com/old/leopards/restaurant/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/old/leopards/restaurant/ui/profile/ProfileFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.old.leopards.restaurant.R
 import com.old.leopards.restaurant.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
