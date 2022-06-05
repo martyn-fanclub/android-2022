a16d0fb2c65d06846c55ab930d07f21077656278
ArtyomScheredin <mrboxman75@gmail.com>
2022-06-05 13:38:43 +0300

    Clean code

--- app/src/main/java/com/old/leopards/restaurant/ui/authorization/LoginFragment.kt
+++ app/src/main/java/com/old/leopards/restaurant/ui/authorization/LoginFragment.kt
@@ -7,11 +7,6 @@ import androidx.navigation.fragment.findNavController
 import com.old.leopards.restaurant.R
 import com.old.leopards.restaurant.databinding.FragmentAuthorizationLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [RegistrationFragment.newInstance] factory method to
@@ -45,52 +40,4 @@ class LoginFragment : Fragment() {
             navController.navigate(R.id.action_loginFragment_to_navigation_food2)
         }
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_authorization_registration.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistrationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-}/*
-class StartFragment : Fragment() {
-
-    private var _binding: FragmentAuthorizationStartBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentAuthorizationStartBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val navController = findNavController()
-        binding.btnLogin.setOnClickListener {
-            navController.navigate(R.id.action_fragment_authorization_start_to_fragment_authorization_login)
-        }
-        binding.btnLogin.setOnClickListener {
-            navController.navigate(R.id.action_fragment_authorization_start_to_fragment_authorization_registration)
-        }
-    }
 }
-*/
