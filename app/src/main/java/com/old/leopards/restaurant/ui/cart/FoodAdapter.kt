8207649d86dea993e58f6601242197c238d77cc8
Artyom Trofimov <troart@bk.ru>
2022-06-09 10:10:04 +0300

    change rv adapter to list adapter

--- app/src/main/java/com/old/leopards/restaurant/ui/cart/FoodAdapter.kt
+++ app/src/main/java/com/old/leopards/restaurant/ui/cart/FoodAdapter.kt
@@ -1,34 +1,36 @@
 package com.old.leopards.restaurant.ui.cart
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.old.leopards.restaurant.R
 import com.old.leopards.restaurant.models.Food
 
-class FoodAdapter(var food: MutableList<Pair<Food, Int>>) : RecyclerView.Adapter<FoodViewHolder>() {
+class FoodAdapter(private val onClick: (Pair<Food, Int>) -> Unit) :
+    ListAdapter<Pair<Food, Int>, FoodViewHolder>(
+        FlowerDiffCallback
+    ) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.cart_item, parent, false)
-        return FoodViewHolder(view)
+        return FoodViewHolder(view, onClick)
     }
 
     override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
-        val food = food[position]
+        val food = getItem(position)
         holder.bind(food)
         holder.itemView.tag = food
     }
-
-    override fun getItemCount(): Int {
-        return food.size
-    }
 }
 
-class FoodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+class FoodViewHolder(itemView: View, val onClick: (Pair<Food, Int>) -> Unit) :
+    RecyclerView.ViewHolder(itemView) {
     private val titleTextView: TextView = itemView.findViewById(R.id.cart_item_title)
     private val descriptionTextView: TextView = itemView.findViewById(R.id.cart_item_description)
     private val priceTextView: TextView = itemView.findViewById(R.id.cart_item_price)
@@ -46,6 +48,7 @@ class FoodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         plusButton.setOnClickListener {
             foodEntry.second.inc()
+            onClick(foodEntry)
         }
 
         minusButton.setOnClickListener {
@@ -53,9 +56,17 @@ class FoodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
                 //TODO
             }
             foodEntry.second.dec()
+            onClick(foodEntry)
         }
+    }
+}
 
+object FlowerDiffCallback : DiffUtil.ItemCallback<Pair<Food, Int>>() {
+    override fun areItemsTheSame(oldItem: Pair<Food, Int>, newItem: Pair<Food, Int>): Boolean {
+        return (oldItem.first.id == newItem.first.id) && (oldItem.second == newItem.second)
     }
 
-
+    override fun areContentsTheSame(oldItem: Pair<Food, Int>, newItem: Pair<Food, Int>): Boolean {
+        return oldItem == newItem
+    }
 }
